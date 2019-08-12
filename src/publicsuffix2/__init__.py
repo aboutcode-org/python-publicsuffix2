8f3177b87ac8272804750aed9a0dc6af4211f500
Philippe Ombredanne <pombredanne@nexb.com>
2019-08-12 15:34:06 +0200

    Format and streamline code and docs
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/publicsuffix2/__init__.py
+++ src/publicsuffix2/__init__.py
@@ -43,7 +43,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import codecs
-import os.path
+from os import path
 import warnings
 
 try:
@@ -54,9 +54,9 @@ except ImportError:
 
 PSL_URL = 'https://publicsuffix.org/list/public_suffix_list.dat'
 
-BASE_DIR = os.path.dirname(__file__)
-PSL_FILE = os.path.join(BASE_DIR, 'public_suffix_list.dat')
-ABOUT_PSL_FILE = os.path.join(BASE_DIR, 'public_suffix_list.ABOUT')
+BASE_DIR = path.dirname(__file__)
+PSL_FILE = path.join(BASE_DIR, 'public_suffix_list.dat')
+ABOUT_PSL_FILE = path.join(BASE_DIR, 'public_suffix_list.ABOUT')
 
 
 
@@ -71,9 +71,10 @@ class PublicSuffixList(object):
         If psl_file is None, the vendored file named "public_suffix_list.dat" is
         loaded. It is stored side by side with this Python package.
 
-        The Mozilla public suffix list is no longer IDNA-encoded, it is UTF-8. For use cases with domains that are IDNA
-        encoded, choose idna=True and the list will be converted upon loading. The wrong encoding will provide incorrect
-        answers in either use case.
+        The Mozilla public suffix list is no longer IDNA-encoded, it is UTF-8.
+        For use cases with domains that are IDNA encoded, choose idna=True and
+        the list will be converted upon loading. The wrong encoding will provide
+        incorrect answers in either use case.
 
         The file format is described at http://publicsuffix.org/
 
@@ -82,22 +83,27 @@ class PublicSuffixList(object):
         """
         # Note: we test for None as we accept empty lists as inputs
         if psl_file is None or isinstance(psl_file, str):
-            with codecs.open(psl_file or PSL_FILE, 'r', 'utf8') as psl:
+            with codecs.open(psl_file or PSL_FILE, 'r', encoding='utf8') as psl:
                 psl = psl.readlines()
         else:
             # assume file-like
             psl = psl_file
+
+        # a list of eTLDs with their modifiers, e.g., *
+        self.tlds = []
         root = self._build_structure(psl, idna)
         self.root = self._simplify(root)
 
     def _find_node(self, parent, parts):
         """
-        recursive processing of a line of the publc suffix list to build the Trie.  Each line is processed into a
-        dictionary, which may contain sub-Trie, and nodes terminate in node of either 0 or 1 (negate).
+        Processing each line of the publc suffix list recursively to build the
+        Trie.  Each line is processed into a dictionary, which may contain sub-
+        Trie, and nodes terminate in node of either 0 or 1 (negate).
 
-        This method takes the current parent Trie, and searches it for the next part in the line (child). If not found,
-        it adds a node to the Trie, creating a new branch with the [0]. If found, the existing sub-Trie is passed for
-        the next part.
+        This method takes the current parent Trie, and searches it for the next
+        part in the line (child). If not found, it adds a node to the Trie,
+        creating a new branch with the [0]. If found, the existing sub-Trie is
+        passed for the next part.
 
         :param parent: current Trie, form is Tuple (negate, dict of Trie)
         :param parts: list of strings
@@ -106,7 +112,8 @@ class PublicSuffixList(object):
         if not parts:
             return parent
 
-        if len(parent) == 1:  # this initiates the Trie from a new node as [negate, dict()]
+        # this initiates the Trie from a new node as [negate, dict()]
+        if len(parent) == 1:
             parent.append({})
 
         assert len(parent) == 2
@@ -114,17 +121,20 @@ class PublicSuffixList(object):
 
         child = parts.pop()
 
-        child_node = children.get(child, None)  # if child already exists as a node, grab the sub-Trie
+        # if child already exists as a node, grab the sub-Trie
+        child_node = children.get(child, None)
 
-        if not child_node:  # if it doesn't exist, creates a new node and initialized with [0]
+        # if it doesn't exist, creates a new node and initialized with [0]
+        if not child_node:
             children[child] = child_node = [0]
 
         return self._find_node(child_node, parts)
 
     def _add_rule(self, root, rule):
         """
-        initial setup for a line of the public suffix list. If it starts with ! that is a negation operation. this
-        calls the find_node() method recursively to build out the Trie for this rule.
+        Initial setup for a line of the public suffix list. If it starts with !
+        that is a negation operation. this calls the find_node() method
+        recursively to build out the Trie for this rule.
 
         :param root: root Trie
         :param rule: string, line of public suffixlist
@@ -141,7 +151,7 @@ class PublicSuffixList(object):
 
     def _simplify(self, node):
         """
-        condenses the lines of the Trie
+        Condense the lines of the Trie in place.
 
         :param node: node in the Trie, either 0/1 or a subTrie
         :return: simplified Trie, form Tuple
@@ -153,23 +163,28 @@ class PublicSuffixList(object):
 
     def _build_structure(self, fp, idna):
         """
-        build a Trie from the public suffix list. If idna==True, idna-encode each line before building.
+        Build a Trie from the public suffix list. If idna==True, idna-encode
+        each line before building.
 
-        The Trie is comprised of tuples that encode whether the line is a negation line (0 or 1), and terminate with 0.
-        Each node is represented with two-tuple of the form (negate, dict of children / sub-Trie).
-        A partial subTrie therefore looks like:
-        (0, {'ac': 0, 'co': (0, {'blogspot': 0}), 'gv': 0,....})
-        where each tuple starts with the negation encoding, and each leaf in the Trie as a dictionary element returns 0.
+        The Trie is comprised of tuples that encode whether the line is a
+        negation line (0 or 1), and terminate with 0. Each node is represented
+        with two-tuple of the form (negate, dict of children / sub-Trie). A
+        partial subTrie therefore looks like: (0, {'ac': 0, 'co': (0,
+        {'blogspot': 0}), 'gv': 0,....}) where each tuple starts with the
+        negation encoding, and each leaf in the Trie as a dictionary element
+        returns 0.
 
-        Also creates an instance attribute, tlds, which simply contains the publicsuffix list, with the modifiers such
-        as wildcards, as a list. This can be accessed for post-processing by the application.
+        Also creates an instance attribute, tlds, which simply contains the
+        publicsuffix list, with the modifiers such as wildcards, as a list. This
+        can be accessed for post-processing by the application.
 
         :param fp: pointer for the public suffix list
         :param idna: boolean, convert lines to idna-encoded strings
         :return: Trie
         """
         root = [0]
-        tlds = []
+
+        tlds = self.tlds
 
         for line in fp:
             line = line.strip()
@@ -181,17 +196,19 @@ class PublicSuffixList(object):
 
             self._add_rule(root, line.split()[0].lstrip('.'))
 
-        self.tlds = tlds  # a list of eTLDs with their modifiers, e.g., *
         return root
 
     def _lookup_node(self, matches, depth, parent, parts, wildcard):
         """
-        traverses the Trie recursively to find the parts. By default, the traverse follows wildcards, as appropriate for
-        the public suffix list, but if wildcard is set to False, it will stop at wildcard leaves. This can be useful for
-        summarizing complex wildcard domains like those under amazonaws.com.
+        Traverses the Trie recursively to find the parts. By default, the
+        traverse follows wildcards, as appropriate for the public suffix list,
+        but if wildcard is set to False, it will stop at wildcard leaves. This
+        can be useful for summarizing complex wildcard domains like those under
+        amazonaws.com.
 
-        the lookup is tracked via a list, initially set to all None, that marks the negation flags of nodes it matches.
-        each match will be marked for later composition of the SLD.
+        The lookup is tracked via a list, initially set to all None, that marks
+        the negation flags of nodes it matches. each match will be marked for
+        later composition of the SLD.
 
         :param matches: list, parts long, None (initial), 0, or 1
         :param depth: int, how far in the Trie this run is
@@ -219,12 +236,14 @@ class PublicSuffixList(object):
 
     def get_sld(self, domain, wildcard=True, strict=False):
         """
-        gets the second-level-domain (SLD) or private suffix of a given domain according to the public suffix list.
-        The public suffix list includes wildcards, so if wildcard is set to True, this will follow the wildcard on
-        traversal, otherwise it will stop at wildcard nodes.
+        Return the second-level-domain (SLD) or private suffix of a given domain
+        according to the public suffix list. The public suffix list includes
+        wildcards, so if wildcard is set to True, this will follow the wildcard
+        on traversal, otherwise it will stop at wildcard nodes.
 
-        The logic does not check by default whether the TLD is in the Trie, so for example, 'www.this.local' will
-        return 'this.local'. If you want to ensure the TLD is in the public suffix list, use strict=True.
+        The logic does not check by default whether the TLD is in the Trie, so
+        for example, 'www.this.local' will return 'this.local'. If you want to
+        ensure the TLD is in the public suffix list, use strict=True.
 
         :param domain: string, needs to match the encoding of the PSL (idna or UTF8)
         :param wildcard: boolean, follow wildcard patterns
@@ -245,15 +264,21 @@ class PublicSuffixList(object):
                 return '.'.join(parts[i:])
 
     def get_public_suffix(self, domain, wildcard=True, strict=False):
+        """
+        Use get_sld() instead.
+        """
         return self.get_sld(domain, wildcard, strict)
 
     def get_tld(self, domain, wildcard=True, strict=False):
         """
-        gets the TLD, or public suffix, of a domain using the public suffix list. uses wildcards if set, and checks for
-        valid top TLD is strict=True.
+        Return the TLD, or public suffix, of a domain using the public suffix
+        list. uses wildcards if set, and checks for valid top TLD is
+        strict=True.
 
-        this will return the domain itself when it is an ICANN TLD, e.g., 'com' returns 'com', for follow on processing,
-        while 'co.uk' return 'uk'. On the other hand, more complicated domains will return their public suffix, e.g.,
+        This will return the domain itself when it is an ICANN TLD, e.g., 'com'
+        returns 'com', for follow on processing, while 'co.uk' return 'uk'. On
+        the other hand, more complicated domains will return their public
+        suffix, e.g.,
         'google.co.uk' will return 'co.uk'.  Root ('.') will return empty string.
 
         :param domain: string
@@ -264,8 +289,8 @@ class PublicSuffixList(object):
         sld = self.get_sld(domain, wildcard, strict)
         if sld is None:
             return None
-        elif sld.count(".") > 0:  
-            return ".".join(sld.split(".")[1:])
+        elif sld.count('.') > 0:
+            return '.'.join(sld.split('.')[1:])
         else:
             return sld
 
@@ -275,9 +300,10 @@ _PSL = None
 
 def get_sld(domain, psl_file=None, wildcard=True, idna=True, strict=False):
     """
-    Return the private suffix or SLD for a `domain` DNS name string. The original publicsuffix2 library used the method
-    get_public_suffix() for this purpose, but get_private_suffix() is more proper.
-    Convenience function that builds and caches a PublicSuffixList object.
+    Return the private suffix or SLD for a `domain` DNS name string. The
+    original publicsuffix2 library used the method get_public_suffix() for this
+    purpose, but get_private_suffix() is more proper. Convenience function that
+    builds and caches a PublicSuffixList object.
 
     Optionally read, and parse a public suffix list. `psl_file` is either a file
     location string, or a file-like object, or an iterable of lines from a
@@ -295,8 +321,9 @@ def get_sld(domain, psl_file=None, wildcard=True, idna=True, strict=False):
 
 def get_tld(domain, psl_file=None, wildcard=True, idna=True, strict=False):
     """
-    Return the public suffix for a `domain` DNS name string. (this is actually the private suffix that is returned)
-    Convenience function that builds and caches a PublicSuffixList object.
+    Return the TLD or public suffix for a `domain` DNS name string. (this is
+    actually the private suffix that is returned) Convenience function that
+    builds and caches a PublicSuffixList object.
 
     Optionally read, and parse a public suffix list. `psl_file` is either a file
     location string, or a file-like object, or an iterable of lines from a
@@ -314,9 +341,10 @@ def get_tld(domain, psl_file=None, wildcard=True, idna=True, strict=False):
 
 def get_public_suffix(domain, psl_file=None, wildcard=True, idna=True, strict=False):
     """
-    Included for compatibility with the original publicsuffix2 library -- this function returns the private
-    suffix or SLD of the domain. To get the public suffix, use get_tld().
-    Convenience function that builds and caches a PublicSuffixList object.
+    Included for compatibility with the original publicsuffix2 library -- this
+    function returns the private suffix or SLD of the domain. To get the public
+    suffix, use get_tld(). Convenience function that builds and caches a
+    PublicSuffixList object.
 
     Optionally read, and parse a public suffix list. `psl_file` is either a file
     location string, or a file-like object, or an iterable of lines from a
@@ -327,9 +355,12 @@ def get_public_suffix(domain, psl_file=None, wildcard=True, idna=True, strict=Fa
 
     The file format is described at http://publicsuffix.org/
     """
-    warnings.warn("method returns private suffix, SLD, or registrable domain. "
-                  "equivalent to method get_sld(). to get the public suffix itself, use get_tld().",
-                  UserWarning)
+    warnings.warn(
+        'This function returns the private suffix, SLD, or registrable domain. '
+        'This equivalent to function get_sld(). '
+        'To get the public suffix itself, use get_tld().',
+        UserWarning
+    )
     return get_sld(domain, psl_file, wildcard, idna, strict)
 
 
